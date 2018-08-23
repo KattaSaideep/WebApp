a9ccc3cc9dab013a668285940d2497dc0620ce41
17139 <17139@LAPTOP-87RJFHR7>
2018-08-23 17:17:15 -0500

    Web app

---
+++ spring-mvc-example/src/main/java/com/journaldev/spring/controller/HomeController.java
@@ -0,0 +1,35 @@
+package com.journaldev.spring.controller;
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+//import org.apache.catalina.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+
+@Controller
+public class HomeController 
+{
+	@RequestMapping(value="/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model)
+	{
+		System.out.println("Homepage requested, Locale ="+locale);
+		Date date=new Date();
+		DateFormat dateformat=DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG,locale);
+		String formattedDate= dateformat.format(date);
+		model.addAttribute("servertime",formattedDate);
+		return "home";
+	}
+	@RequestMapping(value = "/user", method = RequestMethod.POST)
+	public String user(@Validated User user, Model model) {
+		System.out.println("User Page Requested");
+		model.addAttribute("userName", user.getUserName());
+		return "user";
+	}
+}
